0dbc1b5410bcbd1866f2934c4d25777c2c05b9bf
huss2012 <huss24940@gmail.com>
2024-06-18 06:28:55 +0300

    Install dependinces.

--- island.js
+++ island.js
@@ -1,24 +1,34 @@
 function getNeighbors(row, col, matrix) {
-  
+  let validNeighbors = [];
+  let matrixRowLength = matrix.length;
+  let matrixColumnLength = matrix[0].length;
   // Check top
+  if (row - 1 >= 0 && row - 1 < matrixRowLength & col >= 0 && matrix[row - 1][col] === 1) validNeighbors.push([row - 1, col]);
   // Check top right
+  if (row - 1 >= 0 && row - 1 < matrixRowLength & col + 1 >= 0 && col + 1 < matrixColumnLength && matrix[row - 1][col + 1] === 1) validNeighbors.push([row - 1, col + 1]);
   // Check right
+  if (row >= 0 && col + 1 >= 0 && col + 1 < matrixColumnLength && matrix[row][col + 1] === 1) validNeighbors.push([row, col + 1]);
   // Check bottom right
+  if (row + 1 >= 0 && row + 1 < matrixRowLength & col + 1 >= 0 && col + 1 < matrixColumnLength && matrix[row + 1][col + 1] === 1) validNeighbors.push([row + 1, col + 1]);
   // Check bottom
+  if (row + 1 >= 0 && row + 1 < matrixRowLength & col >= 0 && matrix[row + 1][col] === 1) validNeighbors.push([row + 1, col]);
   // Check bottom left
+  if (row + 1 >= 0 && row + 1 < matrixRowLength & col - 1 >= 0 && col - 1 < matrixColumnLength && matrix[row + 1][col - 1] === 1) validNeighbors.push([row + 1, col - 1]);
   // Check left
+  if (row >= 0 && col - 1 >= 0 && col - 1 < matrixColumnLength && matrix[row][col - 1] === 1) validNeighbors.push([row, col - 1]);
   // Check top left
+  if (row - 1 >= 0 && row - 1 < matrixRowLength & col - 1 >= 0 && col - 1 < matrixColumnLength && matrix[row - 1][col - 1] === 1) validNeighbors.push([row - 1, col - 1]);
   // Return neighbors
-  
+  return validNeighbors;
   // Your code here
 }
 
 function countIslands(matrix) {
-  
+
   // Create a visited set to store visited nodes
   // Initialize count to 0
   // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
+    // If an index contains a 1 and has not been visited,
     // increment island count and start traversing neighbors
       // DO THE THING (increment island count by 1)
       // Initialize a stack with current index
@@ -31,7 +41,7 @@ function countIslands(matrix) {
             // Add neighbor to stack
             // Mark neighbor as visited
   // Return island count
-  
+
   // Your code here
 }
 
